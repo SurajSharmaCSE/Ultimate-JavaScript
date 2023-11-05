f09f4dfba7fd1c39ba5cc222f593afa0abbe8761
SurajSharmaCSE <sharmasurajkumar4649@gmail.com>
2023-11-05 21:41:44 +0530

    chapter1 add

---
+++ 1.Chapter-1/Practice.js
@@ -0,0 +1,33 @@
+//Q1
+// let v = 5;
+// let str = "Suraj";
+
+// console.log(v+str);
+
+// // Q2
+// console.log(typeof(v));
+
+// Q3
+// const obj ={
+//    name: "SUraj",
+//    age:24,
+//    isMaster:false
+// }
+// // obj = 45;  we cant change 
+// // obj = {}; we can do this also
+
+// //Q4
+// obj['friend'] = "Surya";
+// obj['name'] = "Surshar2"
+// console.log(obj)
+
+
+//Q5
+const dict ={
+    dog:'kuta',
+    monkey:'bandar',
+    cat:'bili',
+    ret:'chuha'
+}
+
+console.log(dict['dog']);
