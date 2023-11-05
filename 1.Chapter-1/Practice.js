877c9098ef4440263cec9f82eee2e2fcbfdecc4c
SurajSharmaCSE <sharmasurajkumar4649@gmail.com>
2023-11-05 21:42:27 +0530

    chapter1 add

--- 1.Chapter-1/Practice.js
+++ 1.Chapter-1/Practice.js
@@ -1,25 +1,25 @@
-//Q1
-// let v = 5;
-// let str = "Suraj";
+Q1
+let v = 5;
+let str = "Suraj";
 
-// console.log(v+str);
+console.log(v+str);
 
-// // Q2
-// console.log(typeof(v));
+// Q2
+console.log(typeof(v));
 
-// Q3
-// const obj ={
-//    name: "SUraj",
-//    age:24,
-//    isMaster:false
-// }
-// // obj = 45;  we cant change 
-// // obj = {}; we can do this also
+Q3
+const obj ={
+   name: "SUraj",
+   age:24,
+   isMaster:false
+}
+// obj = 45;  we cant change 
+// obj = {}; we can do this also
 
-// //Q4
-// obj['friend'] = "Surya";
-// obj['name'] = "Surshar2"
-// console.log(obj)
+//Q4
+obj['friend'] = "Surya";
+obj['name'] = "Surshar2"
+console.log(obj)
 
 
 //Q5
